2ad9a8d4e5525a23001ea6bd28df17088f90d6cd
hugopq <hugopq@gmail.com>
2026-05-26 12:38:00 +0100

    uppercase error

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 // src/models/index.js
 const sequelize = require('../config/database');
-const User = require('./User')(sequelize);
+const User = require('./user')(sequelize);
 
 console.log('User model:', typeof User); // Deve ser "function" ou "object"
 console.log('User.findOne:', typeof User.findOne); // Deve ser "function"
